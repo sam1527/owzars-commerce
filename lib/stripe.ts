65a83ecaf62c75d1d8831e3712d0f2e8c0aa6cb0
sam1527 <owzarsllc@gmail.com>
2025-12-10 14:21:23 -0500

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,11 +1,6 @@
+// lib/stripe.ts
 import Stripe from "stripe";
 
-const { STRIPE_SECRET_KEY } = process.env;
-
-if (!STRIPE_SECRET_KEY) {
-  throw new Error("STRIPE_SECRET_KEY is not set. Add it to your environment variables.");
-}
-
-export const stripe = new Stripe(STRIPE_SECRET_KEY, {
-  apiVersion: "2024-06-20",
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2023-10-16",
 });
